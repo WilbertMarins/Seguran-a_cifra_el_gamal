32c2cb8cafc1c75127ef49538dfe81bd3356d046
EnriqueIzel2 <eleaoizel@gmail.com>
2019-11-03 23:32:00 -0400

    update

--- client.py
+++ client.py
@@ -1,37 +1,22 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
-#funcoes especiais
-# envio
-def pre_proc(en_msg1):
-    string = []
-    for i in range(0, len(en_msg1)):
-        string.append(str(en_msg1[i]))
-    msg_pro = "-".join(string)  # msg processada para envio
-    return msg_pro
+from elgamal import gen_key, encrypt, power, decrypt
+from random import randint
 
-
-# recebe
-def pos_pro(msg_ci):
-    processo = msg_ci.split("-")
-    inteiro = []
-    for i in range(0, len(processo)):
-        inteiro.append(int(processo[i]))
-
-    return inteiro
 
 # classe para manipular o socket
-
 class Send:
     def __init__(self):
         self.__msg = ''
         self.new = True
+        self.cifra = list
         self.con = None
 
     def put(self, msg):
         self.__msg = msg
         if self.con != None:
-            # envia um mensagem atraves de uma conexão socket
-            self.con.send(str.encode(self.__msg))
+            # envia um mensagem atravez de uma conexão socket
+            self.con.send(str.encode(str(self.__msg)))
 
     def get(self):
         return self.__msg
@@ -39,9 +24,8 @@ class Send:
     def loop(self):
         return self.new
 
+
 # função esperar - Thread
-
-
 def esperar(tcp, send, host='localhost', port=5000):
     destino = (host, port)
     # conecta a um servidor
@@ -54,91 +38,19 @@ def esperar(tcp, send, host='localhost', port=5000):
         while send.loop():
             # aceita uma mensagem
             msg = tcp.recv(1024)
+            print("antes if", msg)
+            print(str.encode(str(msg, 'utf-8')))
             if not msg:
                 break
-            print(str(msg, 'utf-8'))
 
-################ Algoritmo #####################
+            decifrada = decrypt(msg, gamma, k, p)
+            print("decifrada", type(decifrada))
+            str(decifrada)
 
-import random
-from math import pow
+            print(str(decifrada))
 
-a = random.randint(2, 10)
-
-
-def gcd(a, b):
- if a < b:
-  return gcd(b, a)
- elif a % b == 0:
-  return b;
- else:
-  return gcd(b, a % b)
-
- # Generating large random numbers
-
-
-def gen_key(q):
- key = random.randint(pow(10, 20), q)
- while gcd(q, key) != 1:
-  key = random.randint(pow(10, 20), q)
-
- return key
-
-
-# Modular exponentiation
-def power(a, b, c):
- x = 1
- y = a
-
- while b > 0:
-  if b % 2 == 0:
-   x = (x * y) % c;
-  y = (y * y) % c
-  b = int(b / 2)
-
- return x % c
-
-
-# Asymmetric encryption
-
-def encrypt(msg, q, h, g):
- en_msg = []
-
- k = gen_key(q)  # Public key for sender
- s = power(h, k, q)
- p = power(g, k, q)
-
- for i in range(0, len(msg)):
-  en_msg.append(msg[i])
-
-
- for i in range(0, len(en_msg)):
-  en_msg[i] = s * ord(en_msg[i])
-
- return en_msg, p
-
-
-def decrypt(en_msg, p, key, q):
- dr_msg = []
- h = power(p, key, q)
- for i in range(0, len(en_msg)):
-  dr_msg.append(chr(int(en_msg[i] / h)))
-
- return dr_msg
-
-
-
-
-q = random.randint(pow(10, 20), pow(10, 50))
-g = random.randint(2, q)
-k = gen_key(q)  # Private key for sender
-key = gen_key(q)  # Private key for receiver
-h = power(g, key, q)
-
- ########################
 
 if __name__ == '__main__':
-    
     print('Digite o nome ou IP do servidor(localhost): ')
     host = input()
 
@@ -153,12 +65,21 @@ if __name__ == '__main__':
     processo.start()
     print('')
 
-send.put(k)#envia a chave publica
-msg=input()
-while True:
- send.put(msg)
- msg=input()
+    p = randint(pow(10, 20), pow(10, 50))  # numero primo
+    g = randint(2, p)
+    k = gen_key(p)  # chave privada
+    r = power(g, k, p)  # calcula a congruencia pra fazer algo
+    chave_publica = (p, r, g)
+    send.put(chave_publica)
 
-processo.join()
-tcp.close()
-exit()
+    msg = input()
+    msg_cifrada, gamma = encrypt(msg, p, r, g)
+
+    while True:
+        send.put(msg_cifrada)
+        msg = input()
+        msg_cifrada, gamma = encrypt(msg, p, r, g)
+
+    processo.join()
+    tcp.close()
+    exit()
